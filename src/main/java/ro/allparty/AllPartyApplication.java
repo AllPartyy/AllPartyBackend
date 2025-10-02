ad2867678736bf58f4f89e11d16a35b08af08b45
Florian Ianculescu <florian.ianculescu@gmail.com>
2025-10-03 02:03:58 +0300

    draft initializr

---
+++ src/main/java/ro/allparty/AllPartyApplication.java
@@ -0,0 +1,13 @@
+package ro.allparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllPartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/allparty/AllPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.allparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
